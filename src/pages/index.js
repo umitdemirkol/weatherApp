3a3f2830c47121194e991ec5c767f50c54c8139d
Ümit Uğur Demirkol <umitugurdemirkol@Umit-MacBook-Pro.local>
2023-03-28 15:51:18 +0300

    css designa

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import Weather from './weather'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
